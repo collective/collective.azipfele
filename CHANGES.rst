0624594773c9655321671f3be250b76ca8b65692
Jens W. Klein <jk@kleinundpartner.at>
2014-09-16 11:17:04 +0200

    Preparing release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.1 (unreleased)
+1.1 (2014-09-16)
 ----------------
 
 - removed superfluos dependency on collective.js.angular
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.dev0'
+version = '1.1'
 shortdesc = 'Creates Zip files from Plone or other content asynchronous'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
